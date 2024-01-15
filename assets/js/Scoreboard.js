f4b9a06c09eb80417d746c0a21cb6ef60900a602
Tamerbekir <astoryofame@gmail.com>
2024-01-15 17:00:32 -0500

    fixed wallpaper to make it fullscreen. added function to make clear button clear stats

--- assets/js/Scoreboard.js
+++ assets/js/Scoreboard.js
@@ -2,6 +2,14 @@ let totalScores = localStorage.getItem("totalScores");
 let highScore = document.querySelector("#highScore");
 let clear = document.querySelector("#clear");
 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
 
 totalScores = JSON.parse(totalScores);
 
@@ -15,6 +23,3 @@ if (totalScores !== null) {
 
     }
 }
-goBack.addEventListener("click", function () {
-    window.location.replace("./index.html");
-});
